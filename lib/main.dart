4122160806a9bb7010a434cd2580d459859e0e69
Ghulam Abbass <ghulamabbass.1995@gmail.com>
2019-08-03 13:55:28 +0500

    Created AppBar, NavBar, Swiper

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        primaryColor: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: MyHomePage(title: 'Life Line'),
     );
+
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
   @override
-  _MyPageViewState createState() => _MyPageViewState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-//PageController controller = PageController();
-
-class _MyPageViewState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> {
   PageController _pageController;
 
   @override
@@ -35,64 +36,153 @@ class _MyPageViewState extends State<MyHomePage> {
     _pageController = PageController();
   }
 
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  int _selectedIndex = 0;
+
+  Choice _selectedChoice = choices[0]; // The app's "state".
+
+  void _select(Choice choice) {
+    // Causes the app to rebuild with the new _selectedChoice.
+    setState(() {
+      _selectedChoice = choice;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: PageView(
-          controller: _pageController,
-
-
-          //pageSnapping: false,
-          scrollDirection: Axis.vertical,
-          physics: BouncingScrollPhysics(),
-          children: [
-            Container(
-              color: Colors.red,
-              child: Center(
-                child: RaisedButton(
-                  color: Colors.white,
-                  onPressed: () {
-                    if (_pageController.hasClients) {
-                      _pageController.animateToPage(
-                        1,
-                        duration: const Duration(milliseconds: 400),
-                        curve: Curves.easeInOut,
-                      );
-                    }
-                  },
-                  child: Text('Next'),
-                ),
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              child: Center(
-                child: RaisedButton(
-                  color: Colors.white,
-                  onPressed: () {
-                    if (_pageController.hasClients) {
-                      _pageController.animateToPage(
-                        0,
-                        duration: const Duration(milliseconds: 400),
-                        curve: Curves.easeInOut,
-                      );
-                    }
-                  },
-                  child: Text('Previous'),
-                ),
-              ),
-            ),
-          ],
-
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          // action button
+          IconButton(
+            icon: Icon(choices[0].icon),
+            onPressed: () {
+              _select(choices[0]);
+            },
+          ),
+          IconButton(
+            icon: Icon(choices[1].icon),
+            onPressed: () {
+              _select(choices[1]);
+            },
+          ),
+          PopupMenuButton<Choice>(
+            onSelected: _select,
+            itemBuilder: (BuildContext context) {
+              return choices.skip(2).map((Choice choice) {
+                return PopupMenuItem<Choice>(
+                  value: choice,
+                  child: Text(choice.title),
+                );
+              }).toList();
+            },
+          ),
+        ],
       ),
+      body: PageView(
+        controller: _pageController,
+        //pageSnapping: false,
+        scrollDirection: Axis.vertical,
+        children: [
+          Container(
+            color: Colors.black,
+            child: Center(
+              child: RaisedButton(
+                color: Colors.black,
+                onPressed: () {
+                  if (_pageController.hasClients) {
+                    _pageController.animateToPage(
+                      1,
+                      duration: const Duration(milliseconds: 400),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+                child: Text('Next'),
+
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            child: Center(
+              child: RaisedButton(
+                color: Colors.black,
+                onPressed: () {
+                  if (_pageController.hasClients) {
+                    _pageController.animateToPage(
+                      0,
+                      duration: const Duration(milliseconds: 400),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+                child: Text('Previous'),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Discover'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Inbox'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Profile'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed ,
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          tooltip: "Create Video",
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
+
+class Choice {
+  const Choice({this.title, this.icon});
+  final String title;
+  final IconData icon;
+}
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Home', icon: Icons.short_text),
+  const Choice(title: 'Updates', icon: Icons.short_text),
+  const Choice(title: 'Programs', icon: Icons.short_text),
+  const Choice(title: 'Short Flims', icon: Icons.short_text),
+  const Choice(title: 'Documentry', icon: Icons.short_text),
+  const Choice(title: 'Vlogs', icon: Icons.short_text),
+  const Choice(title: 'Memes', icon: Icons.short_text),
+  const Choice(title: 'Cartoon', icon: Icons.short_text),
+  const Choice(title: 'Radio', icon: Icons.short_text),
+  const Choice(title: 'Blogs', icon: Icons.short_text),
+  const Choice(title: 'Article', icon: Icons.short_text),
+  const Choice(title: 'Reviews', icon: Icons.short_text),
+  const Choice(title: 'Mugic', icon: Icons.short_text),
+  const Choice(title: 'Pictures', icon: Icons.short_text),
+  const Choice(title: 'Nearby', icon: Icons.short_text),
+  const Choice(title: 'ChitChat', icon: Icons.short_text),
+];
+
+
