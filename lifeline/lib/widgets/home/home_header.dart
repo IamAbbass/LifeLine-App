25e8cf2867095378ee66659d01419c1dc301c69f
Ghulam Abbass <ghulamabbass.1995@gmail.com>
2019-09-22 19:49:29 +0500

    Updated

--- lifeline/lib/widgets/home/home_header.dart
+++ lifeline/lib/widgets/home/home_header.dart
@@ -14,11 +14,13 @@ Widget homeHeader() {
 
         Padding(
           padding: EdgeInsets.only(left: 10, right: 10),
-          child: Text(
-            AppStrings.followingString,
-            style: TextStyle(
-                color: Colors.grey, fontSize: 18, fontWeight: FontWeight.w500),
-
+          child: IconButton(
+            icon: new Icon(Icons.directions_run),
+            tooltip: "Following",
+            color: Colors.white,
+            onPressed: () {
+              //Navigator.pushNamed(context, '/Menu');
+            },
           ),
         ),
         Text("|",
@@ -26,21 +28,36 @@ Widget homeHeader() {
                 fontSize: 18, color: Colors.grey, fontWeight: FontWeight.bold)),
         Padding(
           padding: EdgeInsets.only(left: 10, right: 10),
+          child: IconButton(
+            icon: new Icon(Icons.play_for_work),
+            tooltip: "For You",
+            color: Colors.teal,
+            onPressed: () {
+              //Navigator.pushNamed(context, '/Menu');
+            },
+          ),
+
+          /*
           child: Text(AppStrings.forYouString,
               style: TextStyle(
                   fontSize: 18,
                   color: Colors.white,
                   fontWeight: FontWeight.w500)),
+                  */
         ),
         Text("|",
             style: TextStyle(
                 fontSize: 18, color: Colors.grey, fontWeight: FontWeight.bold)),
         Padding(
           padding: EdgeInsets.only(left: 10, right: 10),
-          child: Text(
-            AppStrings.nearByString,
-            style: TextStyle(
-                color: Colors.grey, fontSize: 18, fontWeight: FontWeight.w500),
+          child: IconButton(
+            icon: new Icon(Icons.location_on),
+            tooltip: "For You",
+            color: Colors.white,
+            onPressed: () {
+
+
+            },
           ),
         ),
 
