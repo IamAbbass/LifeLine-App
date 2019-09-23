fa91d1b4b2663ecb369454aad92c0b6b27288126
Ghulam Abbass <ghulamabbass.1995@gmail.com>
2019-09-23 21:05:43 +0500

    Updated inbox.dart

--- lifeline/lib/inbox.dart
+++ lifeline/lib/inbox.dart
@@ -236,3 +236,57 @@ class ChatMessage extends StatelessWidget {
   }
 }
 
+/*
+class FriendDetailsPage extends StatefulWidget {
+  FriendDetailsPage(
+    this.friend, {
+    @required this.avatarTag,
+  });
+
+  final Friend friend;
+  final Object avatarTag;
+
+  @override
+  _FriendDetailsPageState createState() => new _FriendDetailsPageState();
+}
+
+class _FriendDetailsPageState extends State<FriendDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    var linearGradient = const BoxDecoration(
+      gradient: const LinearGradient(
+        begin: FractionalOffset.centerRight,
+        end: FractionalOffset.bottomLeft,
+        colors: <Color>[
+          const Color(0xFF413070),
+          const Color(0xFF2B264A),
+        ],
+      ),
+    );
+
+    return new Scaffold(
+      body: new SingleChildScrollView(
+        child: new Container(
+          decoration: linearGradient,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new FriendDetailHeader(
+                widget.friend,
+                avatarTag: widget.avatarTag,
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: new FriendDetailBody(widget.friend),
+              ),
+              new FriendShowcase(widget.friend),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+*/
